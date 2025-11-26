0b3aab290fae6ea465a1f5c63c735912998fa719
fhosaily <fadlo.hosaily@gmail.com>
2025-11-26 10:58:58 +0100

    more-or-less

--- more-or-less.js
+++ more-or-less.js
@@ -1,16 +1,16 @@
 function more(a){
-    b = a + 1
+    const b = a + 1
     return b
 }
 function less(a){
-    b = a - 1
+    const b = a - 1
     return b
 }
 function add(a,b){
-    c = a + b
+    const c = a + b
     return c
 }
 function sub(a,b){
-    c = a - b
+    const c = a - b
     return c
 }
