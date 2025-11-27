eff2216e73ee4d78db71c348aea562e814437b86
fhosaily <fadlo.hosaily@gmail.com>
2025-11-27 12:45:59 +0100

    is

---
+++ is.js
@@ -0,0 +1,23 @@
+is.num = (arg) => (typeof(arg) === "number");
+
+is.nan = (arg) => Number.isNaN(arg);
+
+is.str = (arg) => (typeof(arg) === "string");
+
+is.bool = (arg) => (typeof(arg) === "boolean");
+
+is.undef = (arg) => arg === undefined;
+
+is.def = (arg) => arg !== undefined;
+
+is.arr = (arg) => Array.isArray(arg);
+
+is.obj = (arg) => (typeof(arg) === "object" && !Array.isArray || typeof(arg) === "null");
+
+is.fun = (arg) => (typeof(arg) === "function");
+
+is.truthy = (arg) => (arg) ? true : false;
+
+is.falsy = (arg) => !is.truthy(arg);
+
+console.log(is.falsy(0))
