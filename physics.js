08c0e2b73769e1e9203f821495c4dbbfc4122f58
fhosaily <fadlo.hosaily@gmail.com>
2025-11-27 13:24:24 +0100

    physics

--- physics.js
+++ physics.js
@@ -1,6 +1,6 @@
 function getAcceleration(){
     if (test.f !== undefined && test.m !== undefined && test.f !== 0 && test.m !== 0){
-        return  test.F / test.m
+        return  test.f / test.m
     } else if (test.Δv !== undefined && test.Δt !== undefined && test.Δv !== 0 && test.Δt !== 0){
         return  test.Δv / test.Δt
     }else if (test.d !== undefined && test.t !== undefined && test.d !== 0 && test.t !== 0){
