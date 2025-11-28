5a017bcb9c854052f445aa7799ec80a9dad38aa4
fhosaily <fadlo.hosaily@gmail.com>
2025-11-28 06:38:00 +0100

    collections

---
+++ collections.js
@@ -0,0 +1,46 @@
+function arrToSet(arr){
+    const uniqueSet = new Set(arr);
+    return uniqueSet
+}
+function arrToStr(arr){
+    const str = arr.join("");
+    return str
+}
+function setToArr(set){
+    const arr = [...set];
+    return arr
+}
+function strToArr(str){
+    const arr = str.Split("");
+    return arr
+}
+function strToSet(str){
+    const arr = strToArr(str);
+    const uniqueSet = arrToSet(arr)
+    return uniqueSet
+}
+function strToArr(str){
+    const arr = str.Split("");
+    return arr
+}
+function mapToObj(map){
+    const obj = Object.fromEntries(map)
+    return obj 
+}
+function objToArr(obj){
+    const arr = Object.values(obj)
+    return arr
+}
+function objToMap(obj){
+    const map = new Map(Object.entries(obj))
+    return map
+}
+function arrToObj(arr){
+    const obj = Object.entries(arr);
+    return obj
+}
+function strToObj(str){
+    const arr = strToArr(str)
+    const obj = arrToObj(arr)
+    return obj
+}
