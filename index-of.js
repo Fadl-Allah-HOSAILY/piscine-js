208e6654e95e92ba2b38b6b07b2b99ce51b09f5e
fhosaily <fadlo.hosaily@gmail.com>
2025-11-28 11:13:16 +0100

    index-of

---
+++ index-of.js
@@ -0,0 +1,43 @@
+function indexOf(arr, value, opt){
+    if (opt === undefined) {
+    opt = 0;
+    }
+    if (opt < 0){
+        opt = arr.length + opt;
+        if (opt < 0) opt = 0
+    }
+    for (let i = opt; i < arr.length; i++){
+        if (arr[i] === value){
+            return i
+        }
+    }
+    return -1
+}
+function lastIndexOf(arr, value, opt){
+    if (opt === undefined) {
+    opt = 0;
+    }
+    if (opt < 0){
+        opt = arr.length + opt;
+        if (opt < 0) opt = 0
+    }
+    let result = 0
+    for (let i = opt; i < arr.length; i++){
+        if (arr[i] === value){
+            result = i
+        }
+    }
+    if (result = 0){
+        return -1
+    }else{
+        return result
+    }
+}
+function includes(arr, value){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === value){
+            return true
+        }
+    }
+    return false
+}
