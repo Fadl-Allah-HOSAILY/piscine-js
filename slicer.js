b0967bc7949ea20ba96d56dcc0cf1a16d47f49a7
fhosaily <fadlo.hosaily@gmail.com>
2025-11-28 12:37:45 +0100

    slicer

--- slicer.js
+++ slicer.js
@@ -1,4 +1,10 @@
 function slice(arr, startIn=0,endIn=arr.length){
+    if (startIn<0){
+        startIn= arr.length+startIn
+    }
+    if (endIn<0){
+        endIn=arr.length+endIn
+    }
     let array = []
     for (let i = startIn; i < endIn; i++){
         array.push(arr[i])
