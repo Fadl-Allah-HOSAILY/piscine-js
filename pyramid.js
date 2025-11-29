a5d10a0ef7056af611088b11bb865e4534ff15ca
fhosaily <fadlo.hosaily@gmail.com>
2025-11-29 12:43:53 +0100

    pyramid

---
+++ pyramid.js
@@ -0,0 +1,10 @@
+function pyramid(str, nbr){
+    let res = ""
+    for (let i = 1; i <= nbr; i++) {
+        let space = nbr - i
+        let chars = 2*i - 1
+        res += " ".repeat(space) + str.repeat(chars) + "\n"
+    }
+    return res.slice(0, res.length - 1)
+}
+
