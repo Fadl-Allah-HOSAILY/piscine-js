51e48cfe162c72931151b65720e60417f142fb54
fhosaily <fadlo.hosaily@gmail.com>
2025-11-29 12:25:44 +0100

    triangle

--- triangle.js
+++ triangle.js
@@ -1,6 +1,6 @@
 function triangle(str, nbr){
     let res = ""
-    for (let i = 0; i <= nbr; i++) {
+    for (let i = 1; i <= nbr; i++) {
         res += str.repeat(i) + "\n"
     }
     return res
