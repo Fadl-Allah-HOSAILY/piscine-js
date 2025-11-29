47c8aae0b7d94c58fd8fefda455df00ebb3825b3
fhosaily <fadlo.hosaily@gmail.com>
2025-11-29 12:50:28 +0100

    get

--- get.js
+++ get.js
@@ -1,29 +1,12 @@
-function get(src, path){
-    if (!path) {
-        return src
-    }
-    let res = recursion(src, path.split("."))
-    return res
-}
-
-function recursion(obj, path){
-    if (path.length === 0){
-        return obj
-    }
-    if (typeof(obj[path[0]]) == "object" && obj[path[0]] != null){
-        return recursion(obj[path[0]], path.slice(1))
-    }else if (obj[path[0]] == null){
-        return undefined
-    }else if (typeof(obj[path[0]]) == "function"){
-        if (obj[path[1]] != undefined){
-            return obj[path[0]].toString
+const get = (src,path) => {
+    let keys = path.split(".");
+    let current = src;
+    for (let key of keys) {
+        if ((current === null) || !(key in current)) {
+            return undefined;
         }
+        current = current[key];
     }
-        return obj[path[0]]
-}
+    return current;
+};
 
-// const t = (x) => x + 1 
-// // console.log(get({ key: 'value' }, 'key'));
-// console.log(get({ a: [{ b: t }] },'a.0.b.toString'));
-// console.log(get({ a: [{ b: t }] },'a.0.b'));
-// // console.log(get({ a: [{ b: 10 }] }, "a.0.b"));
