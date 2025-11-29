f61e75c85f037e371f9dd7b3ea327166410683a6
fhosaily <fadlo.hosaily@gmail.com>
2025-11-29 09:09:30 +0100

    unbreakable

---
+++ unbreakable.js
@@ -0,0 +1,28 @@
+function split(str, flag){
+    if (flag == undefined){
+        flag = ""
+    }
+    const arr = []
+    let index = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str.slice(i, i + flag.length) === flag){
+            arr.push(str.slice(index, i))
+            index = i + flag.length
+            i = i + flag.length
+        }
+    }
+    arr.push(str.slice(index, str.length))
+    return arr
+}
+
+function join(arr, flag){
+    if (flag == undefined){
+        flag = ""
+    }
+    let str = ""
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i] + flag
+    }
+    return str
+    
+}
