511b436d855ff8d8d5fd18d31644f29bb2b450ef
fhosaily <fadlo.hosaily@gmail.com>
2025-11-29 08:23:08 +0100

    reverser

--- reverser.js
+++ reverser.js
@@ -1,7 +1,14 @@
 function reverse(arr){
+    if (Array.isArray){
     const array = []
     for (let i = arr.length-1; i >= 0 ; i--) {
         array.push(arr[i])
     }
     return array
+    }
+    let str = ""
+    for (let i = arr.length-1; i >= 0 ; i--) {
+        str += arr[i]
+    }
+    return str
 }
