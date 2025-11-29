117514f3ab37dcc4030d7ff07641778bfb3598d4
fhosaily <fadlo.hosaily@gmail.com>
2025-11-29 10:01:53 +0100

    nasa

---
+++ nasa.js
@@ -0,0 +1,15 @@
+function nasa(N){
+    let res = ""
+    for (let i = 1; i <= N; i++) {
+        if (i % 5 === 0 && i % 3 === 0 ){
+            res += "NASA"
+        }else if (i % 5 === 0 ){
+            res += "SA"
+        }else if ( i % 3 === 0 ){
+            res += "NA"
+        }else {
+            res += i
+        }
+    }
+    return res
+}
