56b7905e93cbb1744586261d9dd7391fa4ad8309
fhosaily <fadlo.hosaily@gmail.com>
2025-11-29 14:25:16 +0100

    flat

--- flat.js
+++ flat.js
@@ -1,10 +1,11 @@
-function flat(arr){
+function flat(arr, index = 1){
     let temp = []
     for (let i = 0; i < arr.length; i++) {
         if (Array.isArray(arr[i])){
-            temp.push(...flat(arr[i]))
+            temp.push(...flat(arr[i], index -1))
         }else{
             temp.push(arr[i])
         }
     }return temp
 }
+
