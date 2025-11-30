45a52f1c125608ca13ee52cf5deedea76d8f8983
fhosaily <fadlo.hosaily@gmail.com>
2025-12-01 00:01:38 +0100

    letter-space-number

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,7 +1,8 @@
 function letterSpaceNumber(str){
     const reg = /[a-zA-z] \d\b(?![a-zA-Z])/g;
-    let arr = str.match(reg) 
+    let arr = str.match(reg) || []
     return arr
 }
 
 console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.')); 
+console.log(letterSpaceNumber('I like 7up.'));
