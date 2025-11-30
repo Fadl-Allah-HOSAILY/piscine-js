8d63e15d9ad71a87e7a18a9dcccc2b4c6e15b5aa
fhosaily <fadlo.hosaily@gmail.com>
2025-12-01 00:50:40 +0100

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,5 @@
 function sameAmount(str, reg1, reg2){
     let arr1 = str.match(reg1) || []
     let arr2 = str.match(reg2) || []
-    if (arr1.length === arr2.length){
-        return true
-    }
-    return false
+    return arr1.length === arr2.length
 }
