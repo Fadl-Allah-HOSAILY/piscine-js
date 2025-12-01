505577d64af7dbade549b97ec70bd2a6da93a662
fhosaily <fadlo.hosaily@gmail.com>
2025-12-01 01:37:10 +0100

    ion-out

--- ion-out.js
+++ ion-out.js
@@ -1,5 +1,10 @@
 function ionOut(str){
     const reg = /\w*t(?=ion)\w*/g;
     let arr = str.match(reg) || []
-    return arr
+    let array =[]
+    for (let i = 0; i < arr.length; i++) {
+        array.push(arr[i].replace("ion", ""))
+    }
+    return array
 }
+console.log(ionOut('attention, direction'));
