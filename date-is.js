bdbc0e6702b31f93c6d5253e5af1444982cce89e
fhosaily fhosaily <fadlo.hosaily@gmail.com>
2025-12-03 17:49:23 +0100

    date-is

--- date-is.js
+++ date-is.js
@@ -3,14 +3,21 @@ function isValid(date){
         return false
     }
     const newDate = new Date(date).toString()
-    console.log(newDate);
-    
     if (newDate === "Invalid Date"){
         return false
     }
     return true
 }
 
-function isAfter(){
-    
+function isAfter(date1, date2){
+    return date1 > date2
+}
+
+function isAfter(date1, date2){
+    return date1 < date2
+}
+
+function isPast(date){
+    const now = Date.now()
+    if ((isValid(date)) && (isAfter(now))) return true; 
 }
