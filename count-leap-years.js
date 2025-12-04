ee6117897ea595220d1e5968c3fdb4a0b122ea83
fhosaily fhosaily <fadlo.hosaily@gmail.com>
2025-12-04 15:47:13 +0100

    count-leap-year

---
+++ count-leap-years.js
@@ -0,0 +1,8 @@
+function countLeapYears(date){
+    let newDate = new Date(date)
+    let count = 0
+    for(let i = 1; i < date.getFullYear(); i++){
+        if ((i % 4 === 0 && i % 100 !== 0 ) || (i % 400 === 0))count ++
+    }
+    return count
+}
