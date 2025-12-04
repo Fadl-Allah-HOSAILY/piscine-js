5523914e8db75a140c1cedc4ba0ef903fcb9d468
fhosaily fhosaily <fadlo.hosaily@gmail.com>
2025-12-04 16:06:30 +0100

    day-of-the-year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -3,6 +3,8 @@ function dayOfTheYear(date){
     let format = year.padStart(4,"0")
     let start = new Date(format+"-01-01")
     let diffDate = date - start
-    return diffDate / 1000 /60 / 60 / 24
+    return (diffDate / 1000 / 60 / 60 / 24) + 1
 
 }
+
+console.log(dayOfTheYear(new Date('0001-01-01')));
