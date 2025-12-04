c13e3a5185b0eb2acc1890f203cffb9ba15f2207
fhosaily fhosaily <fadlo.hosaily@gmail.com>
2025-12-04 16:04:29 +0100

    day-of-the-year

---
+++ day-of-the-year.js
@@ -0,0 +1,8 @@
+function dayOfTheYear(date){
+    let year = date.getUTCFullYear() + ""
+    let format = year.padStart(4,"0")
+    let start = new Date(format+"-01-01")
+    let diffDate = date - start
+    return diffDate / 1000 /60 / 60 / 24
+
+}
