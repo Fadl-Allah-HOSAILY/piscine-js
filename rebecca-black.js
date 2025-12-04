7ecc56614eb1f37fbba28047ab0773971055f56f
fhosaily fhosaily <fadlo.hosaily@gmail.com>
2025-12-04 14:30:41 +0100

    rebecca-black

---
+++ rebecca-black.js
@@ -0,0 +1,24 @@
+function isFriday(date){
+    let newDate = new Date(date)
+    if (newDate.getDay() === 5) return true 
+    return false
+}
+function isWeekend(date){
+    let newDate = new Date(date)
+    if ((newDate.getDay() === 6) || (newDate.getDay() === 0)) return true 
+    return false
+}
+
+function isLeapYear(date){
+    let newDate = new Date(date)
+    if (newDate.getFullYear() % 4 === 0)return true
+    return false
+}
+
+function isLastDayOfMonth(date){
+    let nextMonth = new Date(date.getFullYear(), date.getMonth()+1,1)
+    let isLastDayOfMonth = nextMonth - 1
+    if (date.getDate() === isLastDayOfMonth.getDate())return true
+    return false
+}
+
