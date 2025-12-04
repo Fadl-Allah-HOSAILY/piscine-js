f50f55bca1e08968cf70384bf77f2163614a8bee
fhosaily fhosaily <fadlo.hosaily@gmail.com>
2025-12-04 10:51:48 +0100

    get-some-time

---
+++ get-some-time.js
@@ -0,0 +1,15 @@
+function firstDayWeek(week, year){
+    let format = year + "-01-01"
+    let date = new Date(format)
+    let day = date.getDay()
+    if (day === 0){
+        day = 7
+    }
+    date.setDate(date.getDate() - (day - 1) + ((week -1) * 7))
+    if (date.getFullYear() < year){
+        return "01-01-" + year
+    }
+ return  `${String(date.getDate()).padStart(2,'0')}-${String(date.getMonth()+1).padStart(2,'0')}-${String(date.getFullYear()).padStart(4,'0')}`
+}
+
+console.log(firstDayWeek(2, "0001"));
