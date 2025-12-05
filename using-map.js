19794bace3df21b08a0553c25a1713927d0b103f
fhosaily <fadlo.hosaily@gmail.com>
2025-12-05 10:18:16 +0100

    using-map

---
+++ using-map.js
@@ -0,0 +1,41 @@
+function citiesOnly(arr){
+    return arr.map(obj => obj.city)
+}
+
+function upperCasingStates(arr){
+    return arr.map(str => {
+        let newArr = str.split(" ").map(s => {
+            return s[0].toUpperCase() + s.slice(1)
+        })
+        return newArr.join(" ")
+    })
+}
+
+function fahrenheitToCelsius(arr){
+    return arr.map(eldegm => {
+        return Math.floor(+deg.slice(0, deg.length -2 - 32) * 5 / 9) + "°C"
+    })
+}
+
+function trimTemp(arr){
+    return arr.map(temp =>{
+         const obj = {
+            city : temp.city,
+            temperature : temp.temperature.split(" ").join("")
+        }
+        return obj
+    })
+}
+
+function tempForecasts(arr){
+    function toCel(str){
+        str = str.split(" ").join("")
+        str = str.slice(0, str.length - 2)
+        const fahren = +str
+        return Math.floor((fahren - 32) * 5 / 9) + "°Celsius"
+    }
+    return arr.map(obj => {
+        return `${toCel(obj.temperature)} in ${obj.city}, ${obj.state}`
+        
+    })
+}
