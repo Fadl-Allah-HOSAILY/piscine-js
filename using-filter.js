77dd559c04207814adc56c3bfd300997f4e88dc2
fhosaily <fadlo.hosaily@gmail.com>
2025-12-06 07:08:44 +0100

    using-filter

---
+++ using-filter.js
@@ -0,0 +1,44 @@
+function filterShortStateName(arr) {
+    return arr.filter(str => (str.length < 7))
+}
+
+function filterStartVowel(arr) {
+    function vowel(str) {
+        if (str[0] === "a" || str[0] === "e" || str[0] === "i" || str[0] === "o" || str[0] === "u") {
+            return true
+        }
+        return false
+    }
+    return arr.filter(str => (vowel(str)))
+}
+
+const regexp = /[aeiou]/gi;
+
+function filter5Vowels(arr) {
+    return arr.filter(str => {
+        const array = str.match(regexp) || []
+        return array.length >= 5
+    })
+}
+
+function filter1DistinctVowel(arr) {
+    return arr.filter(str => {
+        str = str.toLowerCase()
+        const array = str.match(regexp) || []
+        const unique = new Set(array)
+        return unique.size === 1
+    })
+}
+
+function multiFilter(arr) {
+    function filterStartVowel(str) {
+        str = str.toLowerCase();
+        return ["a", "e", "i", "o", "u"].includes(str[0]);
+    }
+    return arr.filter(obj => {
+        return obj.capital.length >= 8 &&
+            !filterStartVowel(obj.name) &&
+            /[aeiou]/i.test(obj.tag) &&
+            obj.region !== "South"
+    })
+}
