2e36f548ef518660b90adb179d7b75a72533b3bc
fhosaily <fadlo.hosaily@gmail.com>
2025-12-06 08:13:58 +0100

    using-reduce

--- using-reduce.js
+++ using-reduce.js
@@ -1,24 +1,23 @@
-function adder(arr, ...args){
-    const array = arr.concat(args)
-    return array.reduce((sum, num) =>{
-        return sum += num
-    }, 0)
+function adder(arr, ...args) {
+    const sumArr = arr.reduce((sum, num) => sum + num, 0)
+    const sumArgs = args.reduce((sum, num) => sum + num, 0)
+    return sumArr + sumArgs
 }
 
-function sumOrMul(arr){
-    return arr.reduce((res, num) =>{
-        if (num % 2 === 0){
-            res .mul *= num
-        }else {
+function sumOrMul(arr) {
+    return arr.reduce((res, num) => {
+        if (num % 2 === 0) {
+            res.mul *= num
+        } else {
             res.sum += num
         }
         return res
-    },{sum : 0, mul: 1})
+    }, { sum: 0, mul: 1 })
 }
 
-function funcExec(arr){
+function funcExec(arr) {
     return arr.reduce((res, func) => {
-        res = func(res) 
+        res = func(res)
         return res
     }, 0)
 }
