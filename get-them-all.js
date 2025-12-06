5705bab12e9cc8c55a8901956a867cfe569f9ce3
fhosaily <fadlo.hosaily@gmail.com>
2025-12-06 14:08:21 +0100

    get-them-all

--- get-them-all.js
+++ get-them-all.js
@@ -1,39 +1,39 @@
-function getArchitects(){
-    const architects = [...document.getElementsByTagName('a')];
-    const nonArchitects = [...document.querySelectorAll("span")]
-    return [architects,[nonArchitects]]
+export function getArchitects(){
+    let architects = [...document.getElementsByTagName('a')];
+    let nonArchitects = [...document.querySelectorAll("span")]
+    return [architects,nonArchitects]
 }
 
-function getClassical(){
-    const architects = getArchitects()
-    const classical = architects.filter(elem =>{
+export function getClassical(){
+    let architects = getArchitects()
+    let classical = architects.filter(elem =>{
         elem.classList.contains("classical")
     })
-    const nonClassical = architects.filter(elem =>{
+    let nonClassical = architects.filter(elem =>{
         !elem.classList.contains("classical")
     })
-    return [classical, [nonClassical]]
+    return [classical, nonClassical]
 }
 
-function getActive(){
-    const architects = getArchitects()
-    const active = architects.filter(elem =>{
+export function getActive(){
+    let architects = getArchitects()
+    let active = architects.filter(elem =>{
         elem.classList.contains("active")
     })
-    const nonActive = architects.filter(elem =>{
+    let nonActive = architects.filter(elem =>{
         !elem.classList.contains("active")
     })
-    return [active, [nonActive]]
+    return [active, nonActive]
 }
 
-function getBonannoPisano(){
-    const bonannoPisano = document.getElementById("BonannoPisano")
-    const classical = architects.filter(elem =>{
+export function getBonannoPisano(){
+    let bonannoPisano = document.getElementById("BonannoPisano")
+    let classical = architects.filter(elem =>{
         elem.classList.contains("classical")
     })
-    const activeClassical = classical.filter(elem =>{
+    let activeClassical = classical.filter(elem =>{
         elem.classList.contains("active")
     })
-    const architects = getArchitects()
-    return [bonannoPisano, [activeClassical]]
+    let architects = getArchitects()
+    return [bonannoPisano, activeClassical]
 }
