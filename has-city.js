9ec7af0e7d6644b42992836b2dac628e7289a900
fhosaily <fadlo.hosaily@gmail.com>
2025-12-06 11:19:17 +0100

    has-city

--- has-city.js
+++ has-city.js
@@ -1,11 +1,15 @@
-function hasCity(county, cities){
-    return function (city){
+function hasCity(county, cities) {
+    return function (city) {
         for (let i = 0; i < cities.length; i++) {
-            if (cities[i] === city){
+            if (cities[i] === city) {
                 return `${city} is a city from ${county}`
-            }else{
-                return `${city} is not a city from ${county}`
             }
         }
-    } 
+        return `${city} is not a city from ${county}`
+    }
 }
+
+const county = "morocco"
+const cities = ["rabat", "oujda", "casablanca", "fes", "marrakech"]
+const city = "taza"
+console.log(hasCity(county, cities)(city));
