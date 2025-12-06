f65c9165dae5de819e0cfbef48355bd43a114b56
fhosaily <fadlo.hosaily@gmail.com>
2025-12-06 09:25:32 +0100

    mapper

--- mapper.js
+++ mapper.js
@@ -9,8 +9,12 @@ function map(arr, func) {
 function flatMap(arr, func) {
     let res = []
     for (let i = 0; i < arr.length; i++) {
-        const mapped = map(arr[i], i, arr)
-        res.push(mapped)
+        const mapped = map(arr, func)
+        if (Array.isArray(mapped)){
+            res.push(...mapped)
+        }else{
+            res.push(mapped)
+        }
     }
     return res
 }
