baab13f2672a87791ddf0405e9ee586b1eda9ab2
fhosaily <fadlo.hosaily@gmail.com>
2025-12-06 08:01:34 +0100

    using-reduce

---
+++ using-reduce.js
@@ -0,0 +1,23 @@
+function adder(arr){
+    return arr.reduce((sum, num) =>{
+        return sum + num
+    }, 0)
+}
+
+function sumOrMul(arr){
+    return arr.reduce((res, num) =>{
+        if (num % 2 === 0){
+            res .mul *= num
+        }else {
+            res.sum += num
+        }
+        return res
+    },{sum : 0, mul: 1})
+}
+
+function funcExec(arr){
+    return arr.reduce((res, func) => {
+        res = func(res) 
+        return res
+    }, 0)
+}
