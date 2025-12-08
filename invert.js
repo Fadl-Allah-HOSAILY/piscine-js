a0442c7876e8e60aadc2d17dbd657781292dac69
fhosaily <fadlo.hosaily@gmail.com>
2025-12-08 15:47:32 +0100

    invert

--- invert.js
+++ invert.js
@@ -1,8 +1,17 @@
 function invert(obj){
     const reverseObj = {}
-    for (const key in obj) {
-        const value = obj[key]
-        reverseObj[value] = key
+    const keys = Object.keys(obj)
+    const values = Object.values(obj)
+    for (let i = 0; i < keys.length; i++) {
+        reverseObj[values[i]] = keys[i]
     }
     return reverseObj
 }
+
+const obj = {
+    a : 1,
+    b : 2,
+    c : 3
+}
+
+console.log(invert(obj));
