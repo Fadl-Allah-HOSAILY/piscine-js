67e8c79693aa1b6e83b42a02b27e76d174ccfd78
fhosaily <fadlo.hosaily@gmail.com>
2025-12-08 16:27:53 +0100

    pick

---
+++ pick-omit.js
@@ -0,0 +1,19 @@
+function pick(obj, arg){
+    const newObj = {}
+    const keys = Object.keys(obj)
+    for (let i = 0; i < obj.length; i++) {
+        if (Array.isArray(arg)){
+            for (let j = 0; j < arg.length; j++) {
+                if (keys[i] === arg[j]){
+                    newObj[keys[i]] = obj[keys[i]]
+                }
+            }
+        }else{
+            if (keys[i] === arg){
+                newObj[keys[i]] === obj[keys[i]]
+            }
+        }
+    }
+    return newObj
+}
+
