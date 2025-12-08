3cc174cfa0753fd2bad98a4cf9ec88fa34d4a4b7
fhosaily <fadlo.hosaily@gmail.com>
2025-12-08 11:58:11 +0100

    pimp

---
+++ pimp-my-style.js
@@ -0,0 +1,38 @@
+export const styles = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'ten',
+  'eleven',
+  'twelve',
+  'thirteen',
+  'fourteen',
+  'fifteen',
+]
+
+let count = 0
+export function pimp(){
+    const button = document.querySelector("button")
+    if (!button.classList.contains("unpimp")){
+        console.log(count);
+        button.classList.add(styles[count])
+        count +=1
+        if (count === styles.length){
+            button.classList.toggle("unpimp")
+        }
+    }else{
+        count -=1
+        console.log(count);
+        button.classList.remove(styles[count])
+        if (count === 0){
+            button.classList.toggle("unpimp")
+        }    
+    }
+    
+}
