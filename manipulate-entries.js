e74f9ed05b9923cb05eab9700caa7a522c3bcc42
fhosaily <fadlo.hosaily@gmail.com>
2025-12-09 16:58:57 +0100

    manipulate-entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -51,7 +51,7 @@ function cartTotal(cart) {
     return mapEntries(cart, ([k, v]) => {
         let newObj = {}
         for (let nutriment in nutritionDB[k]) {
-            newObj[nutriment] = +((v / 100) * nutritionDB[k][nutriment].toFixed(1))
+            newObj[nutriment] = +((v / 100) * nutritionDB[k][nutriment].toFixed(2))
         }
         return [k, newObj]
     })
