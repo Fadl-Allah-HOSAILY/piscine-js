022093e153667e2527aba2d82523913d1aeee349
fhosaily <fadlo.hosaily@gmail.com>
2025-12-09 12:37:55 +0100

    manipulate-keys

---
+++ manipulate-keys.js
@@ -0,0 +1,27 @@
+function filterKeys(obj, func){
+    const filtredObj = {}
+    for (const key in obj) {
+        if (func(key)){
+            filtredObj[key] = obj[key]
+        }
+    }
+    return filtredObj
+}
+
+function mapKeys(obj, func){
+    const mappedObj = {}
+    for (const key in obj) {
+        mappedObj[func(key)] = obj[key]
+    }
+    return mappedObj
+}
+
+function reduceKeys(obj, func, init = ''){
+    const keys = Object.keys(obj)
+    const res = init
+    for (let i = 0; i < keys.length; i++) {
+        res = func(res, keys[i])
+    }
+    return res
+}
+
