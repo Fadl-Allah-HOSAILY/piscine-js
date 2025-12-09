46036fe4c22a472e8091911685c1e4de5035df2f
fhosaily <fadlo.hosaily@gmail.com>
2025-12-09 09:28:02 +0100

    fusion

--- fusion.js
+++ fusion.js
@@ -4,6 +4,7 @@ function fusion(obj1, obj2){
     const keys2 = Object.keys(obj2)
     const value2 = Object.values(obj2)
     const newObj = {}
+    let found = false 
     for (let i = 0; i < keys1.length; i++) {
         let keyI = keys1[i]
         let valueI = value1[i]
@@ -13,19 +14,28 @@ function fusion(obj1, obj2){
             if (keyI === keyJ){
                 if ((Array.isArray(valueI)) && (Array.isArray(valueJ))){
                     newObj[keyI] = [...valueI, ...valueJ]
+                    found = true
                 }else if ((typeof(valueI) === "string") && typeof(valueJ) === "string"){
                     newObj[keyI] = valueI + ' ' + valueJ
+                    found = true
                 }else if ((typeof(valueI) === "number") && typeof(valueJ) === "number"){
                     newObj[keyI] = valueI + valueJ
+                    found = true
                 }else if (((typeof(valueI) === "object") && (!Array.isArray(valueI))) && ((typeof(valueJ) === "object") && (!Array.isArray(valueJ)))){
                     ewObj[keyI] = fusion(obj1[keyI], obj2[keyJ])
+                    found = true
                 }else {
                     newObj[keyI] = valueJ
+                    found = true
                 }
             }
         }
+        if (!found){
+            newObj[keyI] = valueI
+        }
+        found = false
     }
     return newObj
 }
 
-console.log((fusion({ nbr: 12 }, { nbr: 23 }).nbr, 35));
+console.log(fusion({ a: 12, b: 2, c: 43 }, { a: 23, b: 2 }));
