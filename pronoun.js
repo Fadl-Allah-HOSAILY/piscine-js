9a7aed202b6cf4eff52f265dc0f4a0fab0f38739
fhosaily <fadlo.hosaily@gmail.com>
2025-12-10 20:54:28 +0100

    pronouns

--- pronoun.js
+++ pronoun.js
@@ -1,9 +1,11 @@
 function pronoun(str){
-    const pronoun = ["I", "you", "he", "she", "it", "we", "they"]
-    str = str.replace(/[.,;:!?]/g, ' ');
+    const pronoun = ["i", "you", "he", "she", "it", "we", "they"]
+    str = str.replace(/[.,;:!?\n]/g, ' ');
+    
+    str = str.toLowerCase()
+    console.log(str);
     const arr = str.split(" ")
     let res = {}
-    let count = 0
     for (let i = 0; i < arr.length; i++) {
         const word = arr[i];
         if (pronoun.includes(word)){
@@ -19,7 +21,4 @@ function pronoun(str){
     }
     return res
 }
-
-console.log(pronoun(`The seal method seals an object, preventing new properties from being
- added to it and marking all existing properties as non-configurable. Values of present 
-properties can still be changed as long as they are writable.`));
+console.log(pronoun('I buy,\ni to,\nYOU buy,\nit have,\nIt buys,\nit is,\nyou go'));
