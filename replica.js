b85998ab66ed1a5764530ef00b4a1c02cc83887b
fhosaily <fadlo.hosaily@gmail.com>
2025-12-10 19:52:53 +0100

    replica

--- replica.js
+++ replica.js
@@ -1,14 +1,14 @@
-function replica(target, ...source) {
+function replica(target, ...source){
     for (let i = 0; i < source.length; i++) {
         let object = source[i]
         for (const key in object) {
             const value = object[key];
-            if (value && typeof value === "object" && !Array.isArray(value)) {
+            if ((object[key]) && (Object.prototype.toString.call(value) === "object") && (!Array.isArray(object[key]))){
                 target[key] = target[key] || {};
                 replica(target[key], value);
-            } else if (Array.isArray(value)) {
+            }else if (Array.isArray(value)){
                 target[key] = [...value];
-            } else {
+            }else {
                 target[key] = value;
             }
         }
