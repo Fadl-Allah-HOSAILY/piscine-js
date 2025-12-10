d1f7f15bcf8de60e4fdfc9796c2dd87d3a16e791
fhosaily <fadlo.hosaily@gmail.com>
2025-12-10 20:40:20 +0100

    pronouns

--- pronoun.js
+++ pronoun.js
@@ -3,13 +3,13 @@ function pronoun(str){
     let res = {}
     let count = 0
     for (let i = 0; i < str.length; i++) {
-        if (pronoun.includs(str[i])){
+        if (pronoun.includes(str[i])){
             if(!res[str[i]]){
                 res[str[i]] = {word : [], count : 0}
             }
             res[str[i]].count++
             const nextWord = str[i + 1]
-            if(nextWord && pronoun.includs(nextWord)){
+            if(nextWord && pronoun.includes(nextWord)){
             res[str[i]].word.push(nextWord)
             }
         }
