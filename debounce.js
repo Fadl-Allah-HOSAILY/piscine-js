7c87217787f9132f165770b6563d4f87de4816ad
fhosaily <fadlo.hosaily@gmail.com>
2025-12-12 10:48:54 +0100

    debounce

---
+++ debounce.js
@@ -0,0 +1,25 @@
+function debounce(func, delay){
+    let timer
+    return function (...args){
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            func(...args)
+        }, delay)
+    }
+}
+
+function opDebounce(func, delay, leading){
+        let timer
+        return function(...args){
+            if (leading) {
+                if (timer === undefined){
+                    func(...args)
+                    setTimeout(() => {
+                        timer = undefined
+                    }, delay)
+                }
+            }else {
+                debounce(func, delay)
+            }
+        }
+}
