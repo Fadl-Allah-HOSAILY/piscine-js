5b5a7c67650cbad58a44669247b72d4d8bf962ac
fhosaily <fadlo.hosaily@gmail.com>
2025-12-13 12:03:54 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -2,10 +2,10 @@ function interpolation(steps, start, end, callback, duration){
     const distance = (end - start) / steps
     const delay = duration / steps
     for (let i = 0; i < steps; i++) {
-        const x = i * delay
-        const y = start + i * distance
+        const x = start + i * distance
+        const y = i * delay
         setTimeout(() => {
             callback([x, y])
-        }, x)
+        }, y)
     }
 }
